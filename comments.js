74eada6d202210a38de4bd5eeb40ddf03f8583cb
BlazkerAxl <100676226+BlazkerAxl@users.noreply.github.com>
2023-11-01 23:09:41 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -2,63 +2,60 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+const bodyParser = require('body-parser');
 
-// Use the express.static built-in middleware function to serve static files
+//Set up the database
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/comments', {useNewUrlParser: true, useUnifiedTopology: true});
+
+//Set up the schema
+const commentSchema = new mongoose.Schema({
+    name: String,
+    comment: String
+});
+
+//Set up the model
+const Comment = mongoose.model('Comment', commentSchema);
+
+//Set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//Set up the static files
 app.use(express.static('public'));
 
-// Use the express.urlencoded() built-in middleware function to parse urlencoded bodies
-app.use(express.urlencoded({ extended: true }));
+//Set up the body parser
+app.use(bodyParser.urlencoded({extended: true}));
 
-// Use the express.json() built-in middleware function to parse json bodies
-app.use(express.json());
-
-// Use the express.text() built-in middleware function to parse text bodies
-app.use(express.text());
-
-// Use the express.raw() built-in middleware function to parse raw bodies
-app.use(express.raw());
-
-// Use the express.static() built-in middleware function to serve static files
-app.use(express.static('public'));
-
-// Use the express.Router() class to create modular, mountable route handlers
-const router = express.Router();
-
-// Use the router.use() function to load middleware functions
-router.use(function timeLog(req, res, next) {
-    console.log('Time: ', Date.now());
-    next();
+//Set up the routes
+app.get('/', (req, res) => {
+    res.render('home');
 });
 
-// Use the router.get() function to handle GET requests
-router.get('/', function(req, res) {
-    res.send('GET request to the homepage');
-});
+app.get('/comments', (req, res) => {
+    Comment.find({}, (err, comments) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.render('comments', {comments: comments});
+        }
+    });
+}
+);
 
-// Use the router.post() function to handle POST requests
-router.post('/', function(req, res) {
-    res.send('POST request to the homepage');
-});
+app.post('/comments', (req, res) => {
+    Comment.create(req.body.comment, (err, newComment) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect('/comments');
+        }
+    });
+}
+);
 
-// Use the router.put() function to handle PUT requests
-router.put('/', function(req, res) {
-    res.send('PUT request to the homepage');
-});
-
-// Use the router.delete() function to handle DELETE requests
-router.delete('/', function(req, res) {
-    res.send('DELETE request to the homepage');
-});
-
-// Use the router.all() function to handle all HTTP methods and requests
-router.all('/', function(req, res) {
-    res.send('HTTP methods and requests');
-});
-
-// Use the router.param() function to handle parameters
-router.param('id', function(req, res, next, id) {
-    console.log('CALLED ONLY ONCE');
-    next();
-});
-
-// Use the router.route() function to handle multiple routes
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+}
+);
