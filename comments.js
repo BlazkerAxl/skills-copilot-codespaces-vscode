a58c280f92c0584972c6e9f25f60f9135de59152
BlazkerAxl <100676226+BlazkerAxl@users.noreply.github.com>
2023-11-01 23:14:07 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,61 +1,45 @@
-//Create a web server
+//Create web server
 const express = require('express');
-const app = express();
-const port = 3000;
-const path = require('path');
-const bodyParser = require('body-parser');
+//Create router object
+const router = express.Router();
+//Load comment model
+const Comment = require('../models/Comment');
+//Load post model
+const Post = require('../models/Post');
+//Load user model
+const User = require('../models/User');
+//Load passport
+const passport = require('passport');
 
-//Set up the database
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/comments', {useNewUrlParser: true, useUnifiedTopology: true});
+// @route    GET api/comments/test
+// @desc     Tests comments route
+// @access   Public
+router.get('/test', (req, res) => res.json({ msg: 'Comments Works' }));
 
-//Set up the schema
-const commentSchema = new mongoose.Schema({
-    name: String,
-    comment: String
-});
+// @route    POST api/comments
+// @desc     Create comment
+// @access   Private
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    //Validation
+    const { errors, isValid } = validateCommentInput(req.body);
 
-//Set up the model
-const Comment = mongoose.model('Comment', commentSchema);
+    //Check validation
+    if (!isValid) {
+      //If any errors, send 400 with errors object
+      return res.status(400).json(errors);
+    }
 
-//Set up the view engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-//Set up the static files
-app.use(express.static('public'));
-
-//Set up the body parser
-app.use(bodyParser.urlencoded({extended: true}));
-
-//Set up the routes
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
-app.get('/comments', (req, res) => {
-    Comment.find({}, (err, comments) => {
-        if(err){
-            console.log(err);
-        }else{
-            res.render('comments', {comments: comments});
-        }
-    });
-}
-);
-
-app.post('/comments', (req, res) => {
-    Comment.create(req.body.comment, (err, newComment) => {
-        if(err){
-            console.log(err);
-        }else{
-            res.redirect('/comments');
-        }
-    });
-}
-);
-
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-}
-);
+    Post.findById(req.body.postId)
+      .then(post => {
+        const newComment = new Comment({
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id,
+          post: req
+        });
+      )
+)
