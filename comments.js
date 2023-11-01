e9e383ba1a8decaa9b773a23c2f918bbeea7e9e8
BlazkerAxl <100676226+BlazkerAxl@users.noreply.github.com>
2023-11-01 22:47:59 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,16 +1,83 @@
 //create web server
-var express = require('express');
-var app = express();
-//create server
-var server = require('http').createServer(app);
-//create socket
-var io = require('socket.io').listen(server);
-//create array
-users = [];
-connections = [];
+//get express module
+const express = require('express');
+//create express app
+const app = express();
+//get http module
+const http = require('http');
+//create http server
+const server = http.createServer(app);
+//get socket.io module
+const io = require('socket.io')(server);
+//get path module
+const path = require('path');
+//get fs module
+const fs = require('fs');
+//get moment module
+const moment = require('moment');
+//get mysql module
+const mysql = require('mysql');
+//get body-parser module
+const bodyParser = require('body-parser');
 
-//server listen
-server.listen(process.env.PORT || 3000);
-console.log('Server running...');
+//create connection pool
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: 'localhost',
+  user: 'root',
+  database: 'comments',
+  password: 'root',
+  port: 8889
+});
 
-//route
+//use body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//use static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//set view engine
+app.set('view engine', 'ejs');
+
+//set views directory
+app.set('views', path.join(__dirname, 'views'));
+
+//set port
+app.set('port', process.env.PORT || 3000);
+
+//get index page
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+//get comments page
+app.get('/comments', (req, res) => {
+  res.render('comments');
+});
+
+//get comments from database
+app.get('/get-comments', (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+    connection.query('SELECT * FROM comments', (err, rows) => {
+      connection.release(); // return the connection to pool
+      if (err) throw err;
+      res.send(rows);
+    });
+  });
+});
+
+//post comment to database
+app.post('/post-comment', (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+    connection.query(
+      'INSERT INTO comments SET ?',
+      {
+        name: req.body.name,
+        comment: req.body.comment,
+        created_at: moment().format('YYYY-MM-DD HH:mm:ss')
+      },
+      (err, rows) => {
